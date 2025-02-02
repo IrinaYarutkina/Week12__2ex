4296745d4943f20a5b4c22d4094960843fe1ba58
Irina.Yarutkina <irinaarutkina606@gmaol.com>
2025-02-02 15:07:01 +0300

    2 пункт сделан

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ const textLength = resultText.innerText.length;
 console.log(`1. Количество символов в тексте: ${textLength}`);
 
 
+const result = resultText.innerText.replace(/;/g, '\n');
+console.log(`2. Текст с переносами строк: \n${result}`);
+
+
+
 
